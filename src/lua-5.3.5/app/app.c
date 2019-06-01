36f7b734318b274b3aa02956da60413e287f73f2
李绍良 <46514593+lishaoliang@users.noreply.github.com>
2019-06-01 17:33:08 +0800

    测试代码

--- src/lua-5.3.5/app/app.c
+++ src/lua-5.3.5/app/app.c
@@ -11,6 +11,10 @@
 #include "luabgfx.c"
 #endif
 
+#if LUA_T_KLB
+#include "lua_t_klb.h"
+#endif
+
 
 static void pre_loadlib(lua_State* L, const char* p_name, lua_CFunction func)
 {
@@ -37,12 +41,21 @@ int app_openlibs_ex(lua_State *L)
     pre_loadlib(L, "math3d", luaopen_math3d);
 #endif
 
+#if LUA_T_KLB
+    pre_loadlib(L, "t_klb", luaopen_t_klb);
+#endif
+
     return 0;
 }
 
 //////////////////////////////////////////////////////////////////////////
 
 #ifndef __W_LUA__
+
+#if defined(_MSC_VER) && !defined(NDEBUG)
+#include "vld.h"
+#endif
+
 int main(int argc, char **argv)
 {
     int ret = lua_main(argc, argv);
